037248bfe842877e9444eb4eee133e47d851f50e
Taxi4you <41004207+Taxi4you@users.noreply.github.com>
2020-04-04 14:06:31 +0300

    Create socket.io.js

---
+++ app/socket.io.js
@@ -0,0 +1,12 @@
+var server = require('./server');
+var io          = require('socket.io')(server);
+
+// Optinal!
+// you may attach socket io to redis, so redis will manage socket io through multiple nodes
+// I will comment those lines, if you need help contact me
+
+// var redis       = require('socket.io-redis');
+// io.adapter(redis({ host: 'localhost', port: 6379 }));
+// io.set('origins', '*:*');
+
+module.exports = io;
